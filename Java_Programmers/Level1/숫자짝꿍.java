0b510c76d684c4bc049838584cfe11de322c093d
beeguriri <kkamboting@gmail.com>
2023-03-17 23:29:08 +0900

    0317_프로그래머스풀기 (시간초과 해결)

--- Java_Programmers/Level1/숫자짝꿍.java
+++ Java_Programmers/Level1/숫자짝꿍.java
@@ -1,8 +1,5 @@
 package Java_Programmers.Level1;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 public class 숫자짝꿍 {
 
     /*두 정수 X, Y의 임의의 자리에서 공통으로 나타나는 정수 k(0 ≤ k ≤ 9)들을 이용하여
@@ -12,38 +9,60 @@ public class 숫자짝꿍 {
     X, Y의 짝꿍이 0으로만 구성되어 있다면, 짝꿍은 0입니다.*/
 
     public String solution(String X, String Y) {
-        String answer ="";
-        int cnt = 0;
 
-        String [] xarr = X.split("");
-        String [] yarr = Y.split("");
+        StringBuilder answer = new StringBuilder();
 
-        Arrays.sort(xarr);
-        Arrays.sort(yarr);
+        int [] strx = {0,0,0,0,0,0,0,0,0,0}; //0~9까지의 정수
+        int [] stry = {0,0,0,0,0,0,0,0,0,0};
 
-        //런타임에러(6.7.8.9.10), 시간초과
-        for(int i = 0; i< xarr.length; i++){
-            for(int j = 0; j<yarr.length; j++) {
-                if(xarr[i].equals(yarr[j])) {
-                    cnt++;
-                    yarr[j] = "";
-                    answer += xarr[i];
-                    break;
-                }
+        //각 정수 count
+        for (String x : X.split(""))
+            strx[Integer.parseInt(x)]++;
+
+        for (String y : Y.split(""))
+            stry[Integer.parseInt(y)]++;
+
+        //내림차순 정렬할거라 마지막인덱스부터 확인
+        for (int i = strx.length-1; i>=0; i--) {
+            while(strx[i] > 0 && stry[i] > 0) {
+                answer.append(i);
+                strx[i]--;
+                stry[i]--;
             }
         }
 
-        if (cnt==0) return "-1";
+        if("".equals(answer.toString())) return "-1";
+        else if(answer.substring(0,1).equals("0")) return "0";
+        else return answer.toString();
 
-        if (Integer.parseInt(answer)==0) return "0";
+        //런타임에러 -> 반복횟수때문인듯..
+//        //length 작은길이를 X에, 긴 길이를 Y에
+//        if (X.length()>Y.length()) {
+//            String temp = Y;
+//            Y = X;
+//            X = temp;
+//        }
+//
+//        //짧은 길이 만큼만 반복문 돌아주면 됨
+//        for (int i = 0; i<X.length(); i++) {
+//            char c = X.charAt(i);
+//            if (Y.contains(String.valueOf(c))) {
+//                cnt++;
+//                answer.append(c);
+//                Y = Y.replaceFirst(String.valueOf(c), "");
+//            }
+//        }
+//
+//        if (cnt==0) return "-1";
+//
+//        if (Integer.parseInt(answer.toString())==0) return "0";
+//
+//        String [] temp = answer.toString().split("");
+//        Arrays.sort(temp, Collections.reverseOrder());
+//        String result = "";
+//        for(String s : temp)
+//            result += s;
 
-        String [] temp = answer.split("");
-        Arrays.sort(temp, Collections.reverseOrder());
-        String result = "";
-        for(String s : temp)
-            result += s;
-
-        return result;
     }
     public static void main(String[] args) {
         숫자짝꿍 a = new 숫자짝꿍();
