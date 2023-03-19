1c5f544462a27d55b6edf6491870f5180086c050
beeguriri <kkamboting@gmail.com>
2023-03-19 18:46:28 +0900

    0319_backjoon_test

---
+++ Java_Baekjoon/test.java
@@ -0,0 +1,24 @@
+package Java_Baekjoon;
+
+import java.util.Scanner;
+
+public class test {
+
+    /*
+    public class Main {
+	    public static void main(String[] args) {
+
+        }
+	}
+}*/
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+    }
+
+}
