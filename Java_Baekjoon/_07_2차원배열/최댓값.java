10220fc24805ef31509c8c7204051f20b9123c32
beeguriri <kkamboting@gmail.com>
2023-03-27 22:07:52 +0900

    0327_2Darray

--- Java_Baekjoon/_07_2차원배열/최댓값.java
+++ Java_Baekjoon/_07_2차원배열/최댓값.java
@@ -3,7 +3,6 @@ package Java_Baekjoon._07_2차원배열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 최댓값 {
