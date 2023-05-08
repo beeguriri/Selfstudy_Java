f8df3ba4d085219e4f1543aaed7d80fad55449f3
beeguriri <kkamboting@gmail.com>
2023-05-08 17:54:17 +0900

    0508_baekjoon_dp

---
+++ Java_Baekjoon/_23_동적계획법/신나는함수실행.java
@@ -0,0 +1,54 @@
+package Java_Baekjoon._23_동적계획법;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 신나는함수실행 {
+
+    public static int dp [][][] = new int [21][21][21];
+
+    public static int w(int a, int b, int c) {
+
+        if(a>=0 && a<=20 && b>=0 && b<=20 && c>=0 && c<=20 && dp[a][b][c]!=0)
+            return dp[a][b][c];
+
+        if (a<=0 || b<=0 || c<=0)
+            return 1;
+
+        if (a>20 || b>20 || c>20)
+            return dp[20][20][20] = w(20,20,20);
+
+        if (a<b && b<c)
+            return dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+
+        return dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder();
+
+        while(!s.equals("-1 -1 -1")){
+
+            StringTokenizer st = new StringTokenizer(s);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            int ans = w(a,b,c);
+            sb.append("w(" + a + ", " + b + ", " + c + ") = ").append(ans).append("\n");
+
+            s = br.readLine();
+
+        }
+
+        System.out.println(sb);
+
+    }
+}
