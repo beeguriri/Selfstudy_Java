2ac49c4f5d6c9ce2f3513c34faabd9f5df965097
beeguriri <kkamboting@gmail.com>
2023-05-13 10:00:10 +0900

    0513_baekjoon_dp

---
+++ Java_Baekjoon/_23_동적계획법/가장긴감소하는부분수열.java
@@ -0,0 +1,39 @@
+package Java_Baekjoon._23_동적계획법;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 가장긴감소하는부분수열 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int [] arr = new int [n];
+        int [] dp = new int [n];
+
+        for(int i=0; i<n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<n; i++) {
+            dp[i] = 1;
+
+            for(int j=0; j<i; j++)
+                if(arr[j] > arr[i] && dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
+        }
+
+//        System.out.println(Arrays.toString(dp));
+        int max = 0;
+        for (int i : dp)
+            max = Math.max(max, i);
+
+        System.out.println(max);
+
+
+    }
+}
